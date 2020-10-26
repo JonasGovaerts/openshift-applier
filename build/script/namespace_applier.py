ca7dc86d66d7ae639416509c8b496dea90926262
Jonas Govaerts <govaertsjonas1@gmail;com>
2020-10-26 10:21:57 -0400

    updated

--- build/script/namespace_applier.py
+++ build/script/namespace_applier.py
@@ -1,91 +1,96 @@
-#!/usr/bin/python
-
+import git
 import sys
 import os
 import time
 import subprocess
 from datetime import datetime
 
+def log(logMessage):
+  now = datetime.now()
+  dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+  print('{"timestamp":"'+dt_string+'","message":"'+logMessage+'"}')
+
 def pause():
-	try:
-	  log("sleeping for " + timer + " seconds")
-          sleep_timer = float(timer)
-	  time.sleep(sleep_timer)
-	except:
-	  log("sleeping for 60 seconds")
-	  time.sleep(60)
+  try:
+    log("sleeping for " + timer + " seconds")
+    sleep_timer = float(timer)
+    time.sleep(sleep_timer)
+  except:
+    log("sleeping for 60 seconds")
+    time.sleep(60)
 
 def gitDefinition():
-	try:
-	  log("Initializing git credentials")
-	  command = "git config --global user.name 'test' && git config --global user.email 'test@test.com'"
-	  output = subprocess.Popen([command], shell=True, stdout=subprocess.PIPE)
-          log("Initialized dummy git credentials")
-	except:
-	  log("Couldn't initialize dummy git credentials")
-	  sys.exit()
+  try:
+    log("Initializing git credentials")
+    with git_repo.config_writer() as git_config:
+      git_config.set_value('user','email', 'test@test.com')
+      git_config.set_value('user','name', 'test')
+    log("Initialized dummy git credentials")
+  except Exception as exception:
+    error = str(exception).replace('\n', ' ').replace('\r', '')
+    log("Something went wrong: "+error)
+    sys.exit()
 
 def gitClone():
-	try:
-	  log("Cloning git repository "+repo+" into /resources/git")
-	  if "github" not in repo:
-  	    command = "git clone https://"+username+":"+password+"@"+repo+" --single-branch -b "+branch+" /resources/git"
-	  else:
-	    command = "git clone https://"+repo+" --single-branch -b "+branch+" /resources/git"
-	  log(command)
-	  output = subprocess.Popen([command], shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-          time.sleep(5)
-	  stdout,stderr = output.communicate()
-	  if stderr.strip():
-            log("Exiting: Could not clone git repository!")
-	    sys.exit()
-          else:
-	    log("Cloned git repositry successfully in /resources/git")
-	except:
-	  sys.exit()
+  try:
+    repo_dir = "/resources/git"
+    log("Cloning git repository "+repo+" into "+repo_dir)
+    if "github" not in repo:
+       git_url = "https://"+username+":"+password+"@"+repo
+       git.Repo.clone_from(git_url, repo_dir)
+    else:
+       git_url = "https://"+repo
+       git.Repo.clone_from(git_url, repo_dir)
+  except Exception as exception:
+    error = str(exception).replace('\n', ' ').replace('\r', '')
+    log("Something went wrong: "+error)
+    sys.exit()
 
 def gitPull():
-	try:
-	  log("Pulling new updates from git...")
-	  command = "git --work-tree=/resources/git/ --git-dir=/resources/git/.git pull origin "+branch
-	  output = subprocess.Popen([command], shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	  stdout,stderr = output.communicate()
-	  if "Already" in stdout:
-	    log("No new changes were found")
-	  else:
-	    log("Pulled latest changes from the git repo "+repo)
-	  command = "git --work-tree=/resources/git/ --git-dir=/resources/git/.git log --pretty=oneline | head -n 1 | awk '{print $1}'"
-	  output = subprocess.Popen([command], shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	  stdout,stderr = output.communicate()
-	  log("Latest commit: "+stdout.strip())
-	except:
-	  log("Couldn't pull new updates from git")
-	  sys.exit()
+  try:
+    log("Pulling new updates from git...")
+    current_commit_id = git_repo.head.commit.hexsha
+    git_repo.remotes.origin.pull()
+    new_commit_id = git_repo.head.commit.hexsha
+    if new_commit_id == current_commit_id:
+        log("No new changes were found")
+        log("Latest commit: "+current_commit_id)
+    else:
+        log("Pulled latest changes from git")
+        log("Latest commit: "+new_commit_id)
+  except Exception as exception:
+    error = str(exception).replace('\n', ' ').replace('\r', '')
+    log("Something went wrong: "+error)
+    sys.exit()
+
+def selectBranch():
+  try:
+    log("Switching to branch "+branch)
+    git_repo.git.checkout(branch)
+  except Exception as exception:
+    error = str(exception).replace('\n', ' ').replace('\r', '')
+    log("Something went wrong: "+error)
+    sys.exit()
 
 def ocApply():
-	try:
-	  findCMD = 'find -L /resources/git'+subdir+' -type f \( -name \*.json -o -name \*.yml -o -name \*.yaml \)'
-	  out = subprocess.Popen(findCMD, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	  stdout,stderr = out.communicate()
-	  filelist = stdout.decode().split()
-
-	  for x in filelist:
-	    command = 'oc apply -f '+x
-	    output= subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	    stdout,stderr = output.communicate()
-	    if not stderr.rstrip():
-	      log(stdout.rstrip())
-	    else:
-	      log(stderr.rstrip())
-	except:
-	  log("Something went wrong, are there any objects available in /resources/git/"+subdir)
-	  sys.exit()
-
-def log(logMessage):
-	now = datetime.now()
-	dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-	print('{"timestamp":"'+dt_string+'",'+'"message":"'+logMessage+'"}')
-
+  try:
+    findCMD = 'find -L /resources/git/'+subdir+' -type f \( -name \*.json -o -name \*.yml -o -name \*.yaml \)'
+    out = subprocess.Popen(findCMD, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout,stderr = out.communicate()
+    filelist = stdout.decode().split()
+  
+    for x in filelist:
+      command = 'oc_apply '+x
+      output= subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+      stdout,stderr = output.communicate()
+      if not stderr.rstrip():
+        log(str(stdout.decode().rstrip()))
+      else:
+        log(str(stderr.decode().rstrip()))
+  except Exception as exception:
+    error = str(exception).replace('\n', ' ').replace('\r', '')
+    log("Something went wrong: "+error)
+    sys.exit()
 
 #### Application Run time #####
 log("openshift-applier is starting...")
@@ -102,10 +107,12 @@ except:
   log("Couln't initialize needed variables, required variables are: USERNAME, PASSWORD, GITREPO, BRANCH, TIMER, DIR")
   sys.exit()
 
+gitClone() # Clone the git repo to the location /resources/git
+git_repo = git.Repo('/resources/git') # initialize the git_repo variable to globally use it
+selectBranch()
 gitDefinition()
-gitClone()
 
 while True:
-  gitPull()
-  ocApply()
-  pause()
+    gitPull()
+    ocApply()
+    pause()
